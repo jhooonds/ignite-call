9a78ca8cd09b7bc87645ef6c9e18a5ae69b80365
jhooonds <jonatan_ds@msn.com>
2024-11-28 23:13:29 -0300

    feat:  create page update-profile

--- app/api/users/time-intervals/route.ts
+++ app/api/users/time-intervals/route.ts
@@ -6,14 +6,17 @@ import { NextRequest, NextResponse } from 'next/server';
 import { z } from "zod";
 
 const timeIntervalBodySchema = z.object({
-  intervals: z.array(
-    z.object({
-      weekDay: z.number(),
-      startTimeInMinutes: z.number(),
-      endTimeInMinutes: z.number(),
-    })
-  ),
+  intervals: z.object({
+    intervals: z.array(
+      z.object({
+        weekDay: z.number(),
+        startTimeInMinutes: z.number(),
+        endTimeInMinutes: z.number(),
+      })
+    ),
+  }),
 });
+
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const session = await auth();
@@ -25,21 +28,26 @@ export async function POST(req: NextRequest, res: NextResponse) {
     );
   }
 
-  const parsedBody = timeIntervalBodySchema.parse(await req.json());
-  const { intervals } = parsedBody;
+  const body = await req.json()
 
-  const result = await Promise.all(
-    intervals.map((interval) => {
-      return prisma.userTimeInterval.create({
+  const parsedBody = timeIntervalBodySchema.parse(body);
+
+  const intervals = parsedBody.intervals.intervals;
+  console.log(intervals)
+
+
+  await Promise.all(
+    intervals.map((interval) =>
+      prisma.userTimeInterval.create({
         data: {
           week_day: interval.weekDay,
           time_start_in_minutes: interval.startTimeInMinutes,
           time_end_in_minutes: interval.endTimeInMinutes,
           user_id: session.user.id!,
         },
-      });
-    })
+      })
+    )
   );
 
-  return NextResponse.json({ success: true, data: result, status: 201 });
+  return NextResponse.json({ success: true }, { status: 201 });
 }
---
+++ app/register/update-profile/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import MultiStepForm from "../_components/muiltStepForm";
+import Image from "next/image";
+import { Textarea } from "@/components/ui/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
+const updateProfileSchema = z.object({})
+
+type UpdateProfileFormData = z.infer<typeof updateProfileSchema>;
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<UpdateProfileFormData>({
+    resolver: zodResolver(updateProfileSchema),
+  });
+ 
+  async function handleUpdateProfile(data: UpdateProfileFormData) {
+  
+   }
+
+  return (
+    <div className="max-w-[35.75rem] mt-20 mx-auto mb-1 px-4">
+      <div className="px-6">
+        <h1 className="leading-relaxed font-bold text-2xl">
+            Defina sua disponibilidade
+        </h1>
+        <p className="text-gray200 mb-6 text-base">
+          Por último, uma breve descrição e uma foto de perfil.
+        </p>
+        <MultiStepForm step={4} />
+      </div>
+
+      <form onSubmit={handleSubmit(handleUpdateProfile)} className="flex flex-col space-y-6 mt-4 p-4 border border-solid border-gray-800 rounded-md bg-gray-900">
+        <div className=" mt-4 flex flex-col gap-2">
+            <Label className="flex flex-col gap-4 text-gray100">Foto de perfil</Label>
+            <Avatar className="w-16 h-16 mt-2">
+                <AvatarImage  src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+
+           
+        </div>
+        <div className="flex flex-col gap-4">
+            <Label className="flex flex-col gap-4  text-gray100" htmlFor="message">Sobre você</Label>
+            <Textarea className="h-32 bg-gray-950 border border-gray-500" id="message"/>
+            <p className="text-sm text-gray200">Fale um pouco sobre você. Isto será exibido em sua página pessoal.</p>
+        </div>
+
+        <Button
+          className="bg-green-800 text-white hover:bg-green-500"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          Finalizar
+          <ArrowRight />
+        </Button>
+      </form>
+    </div>
+  );
+}
